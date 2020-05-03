b52900f640de15dd9c0337d22360ded1c01328b0
jean101 <jeancarlos0310@gmail.com>
2020-05-03 08:58:38 -0700

    haciendo la tarea

---
+++ practicas/info.js
@@ -0,0 +1,62 @@
+//dia semanas
+
+let diaSemanas = [];
+diaSemanas.dia1 = prompt("Dia 1");
+diaSemanas.dia2 = prompt("Dia 2");
+diaSemanas.dia3 = prompt("Dia 3");
+diaSemanas.dia4 = prompt("Dia 4");
+diaSemanas.dia5 = prompt("Dia 5");
+diaSemanas.dia6 = prompt("Dia 6");
+diaSemanas.dia7 = prompt("Dia 7");
+
+//nombre familia
+
+let nombreFamilia = [];
+
+nombreFamilia.nombrePadre = prompt("Nombre de padre");
+nombreFamilia.nombreMadre = prompt("Nombre de la madre");
+nombreFamilia.nombreHermana = prompt("Nombre de la hermana");
+
+//datos personales
+
+let datosPersonales = { };
+datosPersonales.nombreUsuarios = prompt("Nombre completo");
+datosPersonales.fechaNacimiento = prompt("fecha de nacimiento");
+datosPersonales.lugarNacimiento = prompt("lugar de nacimiento");
+datosPersonales.edad = prompt("edad");
+datosPersonales.comida1 = prompt("ponga su comida favorita 1");
+datosPersonales.comida2 = prompt("ponga su comida favorita 2");
+datosPersonales.comida3 = prompt("ponga su comida favorita 3");
+datosPersonales.primerPais = prompt("paises 1");
+datosPersonales.segundoPais = prompt("paises 2");
+datosPersonales.tercerPais = prompt("paises 3");
+datosPersonales.cuartoPais = prompt("paises 4");
+datosPersonales.quintoPais = prompt("paises 5");
+datosPersonales.licenciaConducir = prompt("posees licencia de conducir aceptar=si cancelar=no");
+datosPersonales.estasUniversidad = confirm("Estas en la universidad");
+
+
+
+let artistaFavorito = { 
+nombreArtista: "drake", 
+fechaNacimientoArtista: "24 de octubre 1986",
+edad: 34,
+canciones: ["hotling bling", "energy","started from the buttom"],
+nacionalidad: "americana",
+};
+
+console.log(nombreUsuarios);
+console.log(fechaNacimiento);
+console.log(datosPersonales.quintoPais);
+console.log(artistaFavorito.edad);
+console.log(artistaFavorito.nombreCompleto);
+console.log(artistaFavorito.fechaNacimientoArtista);
+console.log(artistaFavorito.nacionalidad);
+
+
+
+
+
+
+
+
