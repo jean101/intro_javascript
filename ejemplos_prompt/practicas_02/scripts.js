44d373bbf9ab3399032f2b2af7850e8bbbaf8ecb
jean101 <jeancarlos0310@gmail.com>
2020-05-06 16:19:40 -0700

    haciendo asignacion

---
+++ ejemplos_prompt/practicas_02/scripts.js
@@ -0,0 +1,18 @@
+let textoEscrito = document.querySelector("h1#jean");
+console.log(textoEscrito)
+
+const h1 = document.querySelector("h1#jean");
+
+
+let div = document.querySelector("div.phone");
+console.log(div);
+
+const mañana = document.querySelector("div#mañana");
+
+const btnempezar = document.querySelector("button#empezar");
+console.log(btnempezar);
+
+btnempezar.addEventListener("click" ,function (){
+
+
+});
