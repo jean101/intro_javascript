fc28f828acb0c3b68768e8cd6f4716b7b44331a3
jean101 <jeancarlos0310@gmail.com>
2020-05-29 13:07:36 -0700

    contraseñas

---
+++ numeros/numeros.js
@@ -0,0 +1,14 @@
+
+let contraseñaPrincipal = 1235;
+let contraseña = Number(prompt("Digita la contraseña"));
+
+while(contraseñaPrincipal != contraseña) {
+    alert("no adivinaste la contraseña");
+}
+alert("adivinaste la contraseña");
+
+
+
+
+
+
